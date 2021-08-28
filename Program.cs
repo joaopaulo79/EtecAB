fed7566218a7ebbb65af51cd538c04b1c4272a79
João Paulo <joaopdejesus79@gmail.com>
2021-08-28 10:48:30 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EtecAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" ");
+            Console.WriteLine("Etec Adolpho Berezin - Mongaguá");
+            Console.WriteLine(" ");
+            Console.ResetColor();
+        }
+    }
+}
